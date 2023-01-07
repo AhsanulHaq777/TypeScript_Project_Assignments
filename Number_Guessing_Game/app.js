482ef658ec7f97ea43239f837411e464f41c89d5
AhsanulHaq777 <ulhaq.ahsan777@gmail.com>
2023-01-07 23:04:39 +0500

    changed colors and loop through the right guess

--- Number_Guessing_Game/app.js
+++ Number_Guessing_Game/app.js
@@ -1,30 +1,34 @@
 import inquirer from 'inquirer';
-//console.log(randomNumber);
+import chalk from 'chalk';
+let input = true;
 async function guessingNumber() {
-    let randomNumber = Math.floor((Math.random() * 5) + 1);
-    let userGuess;
-    userGuess = (await inquirer.prompt({
-        name: "guessNumber",
-        type: "number",
-        message: "Guess Any Number From 1 To 5 "
-    }));
-    userGuess = Math.floor(userGuess.guessNumber);
-    if (userGuess < 6 && userGuess > 0) {
-        if (userGuess === randomNumber) {
-            console.log(`
+    while (input) {
+        let randomNumber = Math.floor((Math.random() * 5) + 1);
+        let userGuess;
+        userGuess = (await inquirer.prompt({
+            name: "guessNumber",
+            type: "number",
+            message: chalk.yellow("Guess Any Number From 1 To 5 ")
+        }));
+        userGuess = Math.floor(userGuess.guessNumber);
+        if (userGuess < 6 && userGuess > 0) {
+            if (userGuess === randomNumber) {
+                console.log(chalk.green(`
                 CONGRATULATIONS...
                 You Won!
-            `);
-        }
-        else {
-            console.log(`
+            `));
+            }
+            else {
+                console.log(chalk.red(`
                 Computer Wins... 
                 Number was ${randomNumber}
-            `);
+            `));
+                input = false;
+            }
         }
-    }
-    else {
-        console.log("Please enter from 1 to 5");
+        else {
+            console.log("Please enter from 1 to 5");
+        }
     }
 }
 guessingNumber();
--- Number_Guessing_Game/app.ts
+++ Number_Guessing_Game/app.ts
@@ -1,32 +1,37 @@
 import inquirer from 'inquirer';
 import chalk from 'chalk';
 
+let input = true;
 async function guessingNumber(){
+while(input){
     let randomNumber = Math.floor((Math.random() * 5) + 1);
     let userGuess;
     userGuess = (await inquirer.prompt({
         name: "guessNumber",
         type: "number",
-        message: "Guess Any Number From 1 To 5 "
+        message: chalk.yellow("Guess Any Number From 1 To 5 ")
     }));
     userGuess = Math.floor(userGuess.guessNumber);
     if(userGuess < 6 && userGuess > 0) {
         if(userGuess === randomNumber){
-            console.log(`
+            console.log(chalk.green(`
                 CONGRATULATIONS...
                 You Won!
-            `);
+            `));
         }
         else{
-            console.log(`
+            console.log(chalk.red(`
                 Computer Wins... 
                 Number was ${randomNumber}
-            `)
+            `))
+            input = false;
         }
     }
     else{
         console.log("Please enter from 1 to 5");
     }
+}
+   
     
 }
 
