d692e9d75c5886f4e70b90624dd2131ee78109b0
AhsanulHaq777 <ulhaq.ahsan777@gmail.com>
2023-01-07 21:40:38 +0500

    number guessing game

---
+++ Number_Guessing_Game/app.ts
@@ -0,0 +1,33 @@
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+
+async function guessingNumber(){
+    let randomNumber = Math.floor((Math.random() * 5) + 1);
+    let userGuess;
+    userGuess = (await inquirer.prompt({
+        name: "guessNumber",
+        type: "number",
+        message: "Guess Any Number From 1 To 5 "
+    }));
+    userGuess = Math.floor(userGuess.guessNumber);
+    if(userGuess < 6 && userGuess > 0) {
+        if(userGuess === randomNumber){
+            console.log(`
+                CONGRATULATIONS...
+                You Won!
+            `);
+        }
+        else{
+            console.log(`
+                Computer Wins... 
+                Number was ${randomNumber}
+            `)
+        }
+    }
+    else{
+        console.log("Please enter from 1 to 5");
+    }
+    
+}
+
+guessingNumber();
